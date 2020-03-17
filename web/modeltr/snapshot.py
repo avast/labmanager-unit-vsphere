c84fea04b608bea3cd6d03664bad1c2d80d08334
Daněk Tomáš <danek@avast.com>
2020-03-17 12:12:45 +0100

    snapshots are named with uniq names in VC

--- web/modeltr/snapshot.py
+++ web/modeltr/snapshot.py
@@ -14,3 +14,7 @@ class Snapshot(Document):
     _defaults = {
                     'status': 'not_created',
                 }
+
+    def get_uniq_name(self):
+        created_at_str = str(self.created_at).replace(' ', '_')
+        return f'{self.name}_{created_at_str}'
