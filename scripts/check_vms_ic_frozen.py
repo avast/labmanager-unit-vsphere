9e1a26bd2d33f2caed5b1156e3c7aff1b80a9631
Tomáš Daněk <tomas.danek@avast.com>
2022-06-27 11:37:10 +0200

    script fix based on PR

--- scripts/check_vms_ic_frozen.py
+++ scripts/check_vms_ic_frozen.py
@@ -1,21 +1,26 @@
+import argparse
+
 from vcenter import vcenter
 
-vm_to_uuid_dict = {
-    'Win_10_Pro_64b_EN-ss-test_danek-26': '420d4985-a279-1150-2f49-21b336e0e061'
+parser = argparse.ArgumentParser()
+parser.add_argument('templates', type=str, metavar='vm_name', nargs='+', help='supported instant clone templates')
+args = parser.parse_args()
 
-}
+instant_clone_templates = args.templates
+print(f'Will check if following templates are instant clone frozen: {instant_clone_templates}')
 
 vc = vcenter.VCenter()
 vc.connect()
 
-for machine_name, uuid in vm_to_uuid_dict.items():
 
-    m = vc.get_machine_by_uuid(uuid)
+for machine_name in instant_clone_templates:
+
+    m = vc._VCenter__search_machine_by_name(machine_name)
     is_instant_clone_frozen = m.runtime.instantCloneFrozen
     if is_instant_clone_frozen:
         print(f'[OK] Machine {machine_name} is frozen')
     else:
         print(f'[X] Machine {machine_name} is NOT frozen, freezing...')
-        res = vc.freeze_vm(machine_uuid=uuid)
+        res = vc.freeze_vm(machine_uuid=m.config.uuid)
         print(f'    |-> Machine {machine_name} frozen: {res}')
 
